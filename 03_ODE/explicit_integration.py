320cdf8d677120389fb02f3a2f1c88dcbc2f7987
FJFalkner <franz-josef.falkner@mci.edu>
2025-12-02 07:50:06 +0100

    Finished arenstorf orbit and added stability of numerical integration

--- 03_ODE/explicit_integration.py
+++ 03_ODE/explicit_integration.py
@@ -19,6 +19,24 @@ def explicit_Euler(dydt, t_span, y0, n):
 
     return t, y
 
+def implicit_Euler(dydt, t_span, y0, n):
+
+    # step size
+    h = (t_span[1]-t_span[0]) / n
+    # time vector
+    t = np.linspace(t_span[0], t_span[1], n+1)
+    # initialize solution vector
+    y = np.zeros((n+1, len(y0)))
+    # intial conditions
+    y[0,:] = y0
+
+    # *** start integration ***
+    for i in range(n):
+        # explicit Euler
+        y[i+1,:] = 1/(1+h*(-100))*y[i,:]
+
+    return t, y
+
 def midpoint_rule(dydt, t_span, y0, n):
 
     # step size
